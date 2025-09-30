f660efc384f326e5eeecab0d6c86d24cdaf45e81
Billy Bernardino <Tyranitarbilly@gmail.com>
2025-09-30 15:27:14 -0400

    feature: implement multiply() with unit tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,8 @@ from src.calculator import subtract
 def test_subtract():
     assert subtract(5, 2) == 3
 
+from src.calculator import multiply
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
